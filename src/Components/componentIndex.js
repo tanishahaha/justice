90d7804b3697161630e5945e92a73d92af7aaad1
Tanisha Amin <amintanisha17@gmail.com>
2024-01-19 20:26:03 +0530

    judge dashboard

--- src/Components/componentIndex.js
+++ src/Components/componentIndex.js
@@ -1,14 +1,8 @@
 import RolePicking from "./RolePicking";
 import SignIn from "./SignIn";
-<<<<<<< HEAD
-import SignUp from "./SignUp";
-import Navbar from "./Navbar";
-
-export {RolePicking, SignIn, SignUp, Navbar}
-=======
 import ScreenCaseCitizen from "./ScreenCaseCitizen"
 import ProfileCard from "./ProfileCard";
 import ConnectWalletModal from "./ConnectWalletModal";
+import Navbar from "./Navbar";
 
-export {RolePicking, SignIn,ScreenCaseCitizen , ProfileCard,ConnectWalletModal}
->>>>>>> 5bc5259e4cc83ced1806e7081a929421238309c5
+export {RolePicking, SignIn,ScreenCaseCitizen , ProfileCard,ConnectWalletModal,Navbar}
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
-import {Navbar, RolePicking, SignIn, SignUp} from '../Components/componentIndex'
-=======
-import {RolePicking, SignIn,ScreenCaseCitizen,ProfileCard, ConnectWalletModal} from '../Components/componentIndex'
->>>>>>> 5bc5259e4cc83ced1806e7081a929421238309c5
+import {RolePicking, SignIn,ScreenCaseCitizen,ProfileCard, ConnectWalletModal, Navbar} from '../Components/componentIndex'
 
 export default function Home() {
   return (
     <div>
       {/* <RolePicking/> */}
       {/* <SignIn/> */}
-<<<<<<< HEAD
-      {/* <SignUp/> */}
-      <Navbar/>
-=======
       {/* <ScreenCaseCitizen/> */}
       {/* <ProfileCard/> */}
-      <ConnectWalletModal/>   
->>>>>>> 5bc5259e4cc83ced1806e7081a929421238309c5
+      {/* <ConnectWalletModal/>    */}
+      <Navbar/>
     </div>
   );
 }
